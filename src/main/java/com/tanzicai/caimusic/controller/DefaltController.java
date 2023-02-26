eadef99058ff9f154c2dd21f9aaae880ebf389af
tanzicai <1728561744@qq.com>
2023-02-26 21:18:15 +0800

    初始化项目

---
+++ src/main/java/com/tanzicai/caimusic/controller/DefaltController.java
@@ -0,0 +1,17 @@
+package com.tanzicai.caimusic.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DefaltController {
+
+    @GetMapping("hello")
+    public String sayHello(){
+        return "hello";
+    }
+}
